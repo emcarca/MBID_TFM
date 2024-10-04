38ea53d57b73369580e67d0781388c9ec042623f
emcarca <emcarca@gmail.com>
2024-10-04 10:18:21 +0200

    Revisión final. Código revisado y validado.

--- ObtencionDatos/Obtener_Datos_Demanda.py
+++ ObtencionDatos/Obtener_Datos_Demanda.py
@@ -9,14 +9,15 @@ import pandas as pd
 """ DEFINICIÓN DE FUNCIONES AUXILIARES """
 """ Funciones Globales """
 # 1. Conexión con BD
+# Se omiten los datos de conexión al tratarse de un entorno privado.
 ## Se definen 2 posibles funciones para conexión a BD
 # 1.1 Con PSYCOPG2
 def conexionBDPostgresSQL():
     conexion = psycopg2.connect(
-        dbname="emiliocardona_09mbid",
-        user="admin",
-        password="08rFHGN0j1im68956jwW7yYf",
-        host="formerly-top-toad-iad.a1.pgedge.io",
+        dbname="",
+        user="",
+        password="",
+        host="",
         port="5432"
     )
     return conexion
@@ -25,17 +26,16 @@ def conexionBDPostgresSQL():
 # 1.2 Con SQLALCHEMY(dataframeGeneracion):
 def conexionBD_SQLALCHEMY():
   # Datos de conexión a POSTGRESQL
-  usuario = 'admin'
-  password = '08rFHGN0j1im68956jwW7yYf'
-  servidor = 'formerly-top-toad-iad.a1.pgedge.io'  
+  usuario = ''
+  password = ''
+  servidor = ''  
   puerto = '5432'
-  basedatos = 'emiliocardona_09mbid'
+  basedatos = ''
   # URL de conexión
   urlConexion = f'postgresql+psycopg2://{usuario}:{password}@{servidor}:{puerto}/{basedatos}'
   # Motor conexión Postgresql
   engine = create_engine(urlConexion)
   return engine
-
 
 # 2. Insertar ConsultaAPI
 """ Función para registrar Consulta + Respuesta y almacenar esta información
@@ -97,8 +97,92 @@ def ejecutarComandoSQLSelect(comandoSQL):
     
     
 """ FUNCIONES DEMANDA DE ENERGÍA """
+# 1. Función para INSERTAR en BD (Demanda)
+def Demanda_JSONData_InsertBD(data):
+  # Obtener los campos que interesan
+  datosDemanda = []
 
+  for item in data["included"]:
+    for value_item in item["attributes"]["values"]:
+      itemDemanda = {
+              "valorDemanda": value_item["value"],
+              "fechaDemanda": value_item["datetime"],
+          }
+      datosDemanda.append(itemDemanda)
+
+  #print(datosDemanda)
+  sqlScript = []
+
+  for sqlItem in datosDemanda:
+      sentenciaSQL = f"""INSERT INTO tfm.energiademanda
+      (fechaDemanda, valorDemanda, unidadMedida)
+      VALUES ('{sqlItem['fechaDemanda']}', '{sqlItem['valorDemanda']}', 'MWh');"""
+
+      sqlScript.append(sentenciaSQL)
+
+  # Separar instrucciones por línea
+  insertSQLScript = "\n".join(sqlScript)
+
+  #print(insertSQLScript)
+  print("Insertar registros - ")
+  conexionBD = conexionBDPostgresSQL()
+  # Crear un cursor
+  cursor = conexionBD.cursor()
+
+  # Ejecutar el script SQL
+  cursor.execute(insertSQLScript)
+
+  # Confirmar los cambios
+  conexionBD.commit()
+
+  # Cerrar el cursor y la conexión
+  cursor.close()
+  conexionBD.close()
+  print("Registros insertados - ")
   
-  
-  
+    
 """ Código principal - Descarga de demanda de energía """
+# Descargar histórico de demanda
+import requests
+from datetime import datetime
+
+# URL de la API
+urlDemanda = "https://apidatos.ree.es/es/datos/demanda/evolucion"
+
+# Desde el año 2010 realiar invocación y almacenar datos si no hay error:
+# Parámetros Año Inicio y Fin
+anyoInicio = 2015
+anyoFin = datetime.now().year
+
+# Lista para almacenar los parámetros
+parametros_consulta = []
+
+# Iterar sobre los años desde el año de inicio hasta el año actual
+for anyo in range(anyoInicio, anyoFin + 1):
+  start_date = datetime(anyo, 7, 12)
+  end_date = datetime(anyo, 7, 31, 23, 59)
+
+  # Crear el diccionario de parámetros
+  parametros = {
+      "start_date": start_date.strftime("%Y-%m-%dT%H:%M"),
+      "end_date": end_date.strftime("%Y-%m-%dT%H:%M"),
+      "time_trunc": "day"
+  }
+  print(parametros)
+  # Realizar la solicitud GET a la API
+  response = requests.get(urlDemanda, params=parametros)
+
+  print(response.url.replace("%3A", ":"))
+
+  # Comprobar si la solicitud fue exitosa (código de estado 200)
+  if response.status_code == 200:
+    # Obtener los datos JSON
+    jsonRespuesta = response.json()
+    # Insertar en BD:
+    Demanda_JSONData_InsertBD(jsonRespuesta)
+    jsonRespuestaString = json.dumps(jsonRespuesta)
+  else:
+      print("Error al obtener los datos de la API:", response.status_code, " - ") #, json.dumps(jsonRespuesta))
+      jsonRespuestaString = "Error: "+str(response.status_code)
+  print(jsonRespuestaString)
+  insertarConsultaAPI("Demanda", response.url.replace("%3A", ":") , jsonRespuestaString)
